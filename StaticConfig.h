0707eb9af18a14bdd4007ca1ed974a4230a11361
Serhiy Bilonozhko <Serhii_Bilongozhko@comcast.com>
2024-08-28 10:16:08 -0400

    Make SERIAL_MODE configurable from outside

--- StaticConfig.h
+++ StaticConfig.h
@@ -1,7 +1,10 @@
 #define SRXL 1
 #define SBUS 2
-//#define SERIAL_MODE SRXL
+
+#ifndef SERIAL_MODE
+// #define SERIAL_MODE SRXL
 #define SERIAL_MODE SBUS
+#endif
 
 String PS4_BT_ADDR = "50:54:4E:50:4E:58";
 #define SERIAL_PIN 13
